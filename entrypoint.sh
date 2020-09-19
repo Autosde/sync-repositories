24348e5ccac919b9449123a5ac6abf41505a96f9
Boris Kozel <borisko@il.ibm.com>
2020-09-19 13:43:19 +0300

    testing

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,25 +13,31 @@ if [[ -z "$UPSTREAM_REPO" ]]; then
 fi
 
 echo "UPSTREAM_REPO=$UPSTREAM_REPO"
+echo "TOKEN=$TOKEN"
 #echo "BRANCHES=$BRANCH_MAPPING"
 
+pwd
+ls -al
 git init
-
-#git config --unset-all http."https://github.com/".extraheader || :
-
+#
+##git config --unset-all http."https://github.com/".extraheader || :
+#
 #echo "Resetting origin to: https://$GITHUB_ACTOR:$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY"
 git remote add origin "https://'abc':$TOKEN@github.com/Autosde/manageiq.git"
 
-echo "Adding tmp_upstream $UPSTREAM_REPO"
+#
+#echo "Adding tmp_upstream $UPSTREAM_REPO"
 git remote add tmp_upstream "https://'abc':$TOKEN@github.com/${UPSTREAM_REPO}.git"
-
-echo "Fetching tmp_upstream"
+git remote -v
+#
+#echo "Fetching tmp_upstream"
 git fetch tmp_upstream master
-git remote --verbose
-
-echo "Pushing changings from tmp_upstream to origin"
+#git remote --verbose
+#
+echo "Pushing changing from tmp_upstream to origin"
 #git push origin "refs/remotes/tmp_upstream/${BRANCH_MAPPING%%:*}:refs/heads/${BRANCH_MAPPING#*:}" --force
-
-echo "Removing tmp_upstream"
+#
+git push origin master
+#echo "Removing tmp_upstream"
 git remote rm tmp_upstream
 git remote --verbose
